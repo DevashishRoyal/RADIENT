1a9ac1e7695c1d3665f810a2eb82a503eace73e9
DevashishRoyal <devashish.royalswebtech@gmail.com>
2025-07-29 15:37:48 +0530

    home jsx home css

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
           <Route path="/courses" element={<Courses />} />
           <Route path="/testimonies" element={<TestimonialsSection />} />
           <Route path="/ashram" element={<AshramPage />} />
